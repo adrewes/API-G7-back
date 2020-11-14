a69bd726b9132a482f95cad57cf3f9f457583187
Agustin <adrewes@uade.edu.ar>
2020-11-14 18:12:21 -0300

    CORS para OPTIONS v4

--- routes/index.js
+++ routes/index.js
@@ -41,6 +41,6 @@ module.exports = (app) => {
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
         res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
         res.send(200);
-        
+    })
 
 };
