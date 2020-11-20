4d40ed6dd742eb8bb36a9716c7970280eb4aacb9
Agustin <adrewes@uade.edu.ar>
2020-11-20 03:59:43 -0300

    Fix error en upload

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -48,33 +48,60 @@ exports.uploadFiles = async function (req, res, next) {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
 
     authController.authenticateToken(req, res, REQUIRED_ROLES, function (err, data) {
-        if (res.statusCode == 200) {
-            //console.log("req",req.body);
-            let form = Formidable.IncomingForm()
-            //console.log("form",form);
-            const uploadsFolder = process.env.UPLOAD_DIR;
-            //console.log("uploadFolder",uploadsFolder);
-            form.multiples = true
-            form.uploadDir = uploadsFolder
-            form.maxFileSize = 50 * 1024 * 1024 // 50 MB
-            const folderCreationResult = await checkCreateUploadsFolder(uploadsFolder)
-            if (!folderCreationResult) {
-                return res.json({ ok: false, msg: "The uploads folder couldn't be created" })
+        payload = data
+    })
+
+    if (res.statusCode == 200) {
+        //console.log("req",req.body);
+        let form = Formidable.IncomingForm()
+        //console.log("form",form);
+        const uploadsFolder = process.env.UPLOAD_DIR;
+        //console.log("uploadFolder",uploadsFolder);
+        form.multiples = true
+        form.uploadDir = uploadsFolder
+        form.maxFileSize = 50 * 1024 * 1024 // 50 MB
+        const folderCreationResult = await checkCreateUploadsFolder(uploadsFolder)
+        if (!folderCreationResult) {
+            return res.json({ ok: false, msg: "The uploads folder couldn't be created" })
+        }
+        form.parse(req, async (err, fields, files) => {
+            let myUploadedFiles = []
+            if (err) {
+                console.log('Error parsing the incoming form', err)
+                return res.json({ ok: false, msg: 'Error passing the incoming form' })
             }
-            form.parse(req, async (err, fields, files) => {
-                let myUploadedFiles = []
-                if (err) {
-                    console.log('Error parsing the incoming form', err)
-                    return res.json({ ok: false, msg: 'Error passing the incoming form' })
+            // If we are sending only one file:
+            if (!files.files.length) {
+                const file = files.files
+                if (!checkAcceptedExtensions(file)) {
+                    console.log('The received file is not a valid type')
+                    return res.json({ ok: false, msg: 'The sent file is not a valid type' })
                 }
-                // If we are sending only one file:
-                if (!files.files.length) {
-                    const file = files.files
+                const fileName = encodeURIComponent(file.name.replace(/&. *;+/g, '-'))
+                try {
+                    await fs.renameAsync(file.path, join(uploadsFolder, fileName))
+                    urlFile = await fileService.createFile(fileName)
+                    var fileData = {
+                        name: fileName,
+                        url: urlFile
+                    }
+                    myUploadedFiles.push(fileData)
+
+                } catch (e) {
+                    console.log('Error uploading the file:')
+                    console.log(e)
+                    try { await fs.unlinkAsync(file.path) } catch (e) { }
+                    return res.json({ ok: false, msg: 'Error uploading the file' })
+                }
+            } else {
+                for (let i = 0; i < files.files.length; i++) {
+                    const file = files.files[i]
                     if (!checkAcceptedExtensions(file)) {
                         console.log('The received file is not a valid type')
                         return res.json({ ok: false, msg: 'The sent file is not a valid type' })
                     }
                     const fileName = encodeURIComponent(file.name.replace(/&. *;+/g, '-'))
+
                     try {
                         await fs.renameAsync(file.path, join(uploadsFolder, fileName))
                         urlFile = await fileService.createFile(fileName)
@@ -83,42 +110,17 @@ exports.uploadFiles = async function (req, res, next) {
                             url: urlFile
                         }
                         myUploadedFiles.push(fileData)
-
                     } catch (e) {
-                        console.log('Error uploading the file:')
-                        console.log(e)
+                        console.log('Error uploading the file')
                         try { await fs.unlinkAsync(file.path) } catch (e) { }
                         return res.json({ ok: false, msg: 'Error uploading the file' })
                     }
-                } else {
-                    for (let i = 0; i < files.files.length; i++) {
-                        const file = files.files[i]
-                        if (!checkAcceptedExtensions(file)) {
-                            console.log('The received file is not a valid type')
-                            return res.json({ ok: false, msg: 'The sent file is not a valid type' })
-                        }
-                        const fileName = encodeURIComponent(file.name.replace(/&. *;+/g, '-'))
-
-                        try {
-                            await fs.renameAsync(file.path, join(uploadsFolder, fileName))
-                            urlFile = await fileService.createFile(fileName)
-                            var fileData = {
-                                name: fileName,
-                                url: urlFile
-                            }
-                            myUploadedFiles.push(fileData)
-                        } catch (e) {
-                            console.log('Error uploading the file')
-                            try { await fs.unlinkAsync(file.path) } catch (e) { }
-                            return res.json({ ok: false, msg: 'Error uploading the file' })
-                        }
-                    }
                 }
-                res.json({ ok: true, msg: 'Files uploaded succesfully!', files: myUploadedFiles })
-            })
+            }
+            res.json({ ok: true, msg: 'Files uploaded succesfully!', files: myUploadedFiles })
+        })
 
-        }
+    }
 
-    })
 
 }
